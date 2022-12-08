63eb08a386202d7e957885586895def8b8a35f60
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-08 00:45:22 -0800

    Update dependency_graph_generator.cpp
    moved all of the path related stuff to its own file this makes the main file 66 lines shorter the whole main file is now only 60 lines long.
    this will also allow for easier porting to skele_lib when I get around to doing that [the path related stuff I did will 100% be part of skele_lib some day at that point I will mark these as deprecated but keep them on github.]

--- dependency_graph_generator/dependency_graph_generator.cpp
+++ dependency_graph_generator/dependency_graph_generator.cpp
@@ -6,55 +6,9 @@
 #include "stringThings.h"
 constexpr size_t MAX_LINE_LENG   = 256;
 constexpr size_t MAX_FOLDER_LENG = 3072; 
-char* getNthParentFolderFromPath(const char* path, size_t n, bool useForwardSlash = true) {
-    SET_STARTLINE; 
-    char* buffer = (char*)malloc(strlen(path));
-    if (buffer == NULL) {
-        DEBUG_P(GenerateDebugMessage( "Unable to create buffer"), "getNthParentFolderFromPath", __LINE__, FUNCT_LINE_OFFSET);
-        return NULL; 
-    }
-    size_t ptrindex = 0, numberOfFolders = 0; 
-    while (path[ptrindex] && numberOfFolders <= n) {
-        buffer[ptrindex] = path[ptrindex]; 
-        ptrindex++; 
-        if ((useForwardSlash && path[ptrindex] == '/') || (!useForwardSlash && path[ptrindex] == '\\')) {
-            numberOfFolders++; 
-        }
-    }
-    return buffer; 
-}
-}
-char* getFolderFromFilePath(const char* filepath,  size_t leng, bool useForwardSlash = true) {
-    size_t i = 0; 
-    size_t lastslashPos = 0;
-    char* tmp = static_cast<char*>(malloc(leng)); 
-    if (!tmp) return NULL; 
-    while (filepath[i]) {
-        if ((useForwardSlash && filepath[i] == '/') || (!useForwardSlash && filepath[i] == '\\')) {
-            lastslashPos = i; 
-        }
-        i++;
-    }
-    if(lastslashPos+1 < leng){  
-        for (size_t i = 0; i <= lastslashPos; i++)
-        {
-            tmp[i] = filepath[i]; 
-        }
-        tmp[lastslashPos + 1] = 0; //this should be unnessisary... 
-    }
-    char* buffer =static_cast<char*>(calloc(strlen(tmp),1));
+char* getAllButFirstChar(const char* str, const size_t len) {
+    char* buffer = static_cast<char*>(malloc(len));
     if (buffer) {
-        for (size_t i = 0; i <= strlen(tmp); i++)
-        {
-            buffer[i] = tmp[i]; 
-        }
-    }
-    free(tmp);
-    return buffer; 
-}
-char* getAllButFirstChar(const char *str, const size_t len) {
-    char* buffer = static_cast<char*>(malloc(len)); 
-    if(buffer){
         for (size_t i = 1; i < len; i++)
         {
             buffer[i-1] = str[i];
@@ -62,31 +16,7 @@ char* getAllButFirstChar(const char *str, const size_t len) {
     }
     return buffer; 
 }
-char* getFileNameFromPath(const char* path, const size_t len, bool useforwardSlash = true) {
-    char* tmp = strcpy_u(path);
-    size_t i = 0; 
-    const char* delim = useforwardSlash ? "/" : "\\"; 
-    char* cntx = (char*) malloc(len); 
-    char* tmpToken = strtok_s(tmp, delim, &cntx);
-    char* token = strcpy_u(tmpToken);
-    while (tmpToken) {
-        tmpToken = strtok_s(NULL, delim, &cntx);
 
-        if(tmpToken) token = strcpy_u(tmpToken);
-    }
-    return NULL; 
-}
-size_t getNumberOfFoldersInPath(const char * filepath, bool useForwardSlash = true) {
-    size_t numberOfFolders = 0; 
-    size_t i = 0; 
-    
-    while(filepath[i])
-    {
-        if ((useForwardSlash && filepath[i] == '/') || (!useForwardSlash && filepath[i] == '\\')) numberOfFolders++;
-        i++;
-    }
-    return numberOfFolders; 
-}
 void parseFile(const char path[], std::vector<char*>& pathQue, bool useForwardSlash = true) {
     FILE* file;
     size_t lineNumber = 1;
