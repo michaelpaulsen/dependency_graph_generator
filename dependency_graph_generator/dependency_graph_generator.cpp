50459072307ec0b6a622c41275dd58caabbfcdf7
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-07 18:29:35 -0800

    added getFolderFromFilePath
    getFolderFromFilePath returns the folder that contains the file that is in the current file path.

--- dependency_graph_generator/dependency_graph_generator.cpp
+++ dependency_graph_generator/dependency_graph_generator.cpp
@@ -4,8 +4,42 @@
 #include <memory>
 #include <cstring>
 #include <cstdio>
+
 constexpr size_t MAX_LINE_LENG   = 256;
 constexpr size_t MAX_FOLDER_LENG = 3072; 
+char* getFolderFromFilePath(const char* filepath, const char  slashChar,  size_t leng) {
+    size_t i = 0; 
+    size_t lastslashPos = 0;
+    char* tmp = static_cast<char*>(malloc(leng)); 
+    if (!tmp) return NULL; 
+    //char* buffer = static_cast<char*>(malloc(leng)); 
+    while (filepath[i]) {
+        if (filepath[i] == slashChar) {
+            lastslashPos = i; 
+        }
+        i++;
+    }
+    if(lastslashPos+1 < leng){  
+        for (size_t i = 0; i <= lastslashPos; i++)
+        {
+            tmp[i] = filepath[i]; 
+        }
+        tmp[lastslashPos + 1] = 0; //this should be unnessisary... 
+    }
+    char* buffer =static_cast<char*>(calloc(strlen(tmp),1));
+    if (buffer) {
+        for (size_t i = 0; i <= strlen(tmp); i++)
+        {
+            
+            buffer[i] = tmp[i]; 
+        }
+    }
+    free(tmp);
+    return buffer; 
+}
+
+
+
 char* getAllButFirstChar(const char str[], size_t len) {
     char* buffer = static_cast<char*>(malloc(len)); 
     if(buffer){
